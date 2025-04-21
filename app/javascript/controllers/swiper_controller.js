b138f5321403deaf55d80123aefde382a2835c2c
Albert Nikolli <albert_nikolli@icloud.com>
2025-04-21 18:22:02 +0200

    set the swiper in a stimulus controller

---
+++ app/javascript/controllers/swiper_controller.js
@@ -0,0 +1,26 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="swiper"
+// If using Swiper from CDN (like you are), it's globally available as `Swiper`
+export default class extends Controller {
+  connect() {
+    // Only initialize if element exists
+    if (typeof Swiper !== "undefined") {
+      new Swiper(this.element, {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 10,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev'
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      });
+    } else {
+      console.warn("Swiper is not loaded.");
+    }
+  }
+}
