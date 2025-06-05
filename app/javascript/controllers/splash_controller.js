c958f83c6e1f025d1e985b1e865e5f6f99c07365
Albert Nikolli <albert_nikolli@icloud.com>
2025-06-05 18:25:35 +0200

    checking if this cleares the issue with the splash

--- app/javascript/controllers/splash_controller.js
+++ app/javascript/controllers/splash_controller.js
@@ -5,15 +5,22 @@ export default class extends Controller {
   connect() {
     const hasSeenSplash = sessionStorage.getItem("splashSeen")
 
-    // Only show splash screen on first visit to root
     if (!hasSeenSplash && window.location.pathname === "/") {
       setTimeout(() => {
-        this.element.classList.add("hidden")
-      }, 2000) // fade out after 2 seconds
+        this.hideSplash()
+      }, 2000)
 
       sessionStorage.setItem("splashSeen", "true")
     } else {
+      this.hideSplash(true)
+    }
+  }
+
+  hideSplash(removeCompletely = false) {
+    if (removeCompletely) {
       this.element.remove()
+    } else {
+      this.element.classList.add("hidden")
     }
   }
 }
