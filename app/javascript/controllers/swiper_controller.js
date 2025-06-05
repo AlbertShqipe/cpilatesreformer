1e95d059cba462ae717c73e4d288b53d9e0fe8c5
Albert Nikolli <albert_nikolli@icloud.com>
2025-06-05 14:23:43 +0200

    slowed down the change of the images

--- app/javascript/controllers/swiper_controller.js
+++ app/javascript/controllers/swiper_controller.js
@@ -15,6 +15,7 @@ export default class extends Controller {
         loop: true,
         slidesPerView: 1,
         spaceBetween: 10,
+        speed: 1000,
         autoplay: {
           delay: 3000,
           disableOnInteraction: false,
